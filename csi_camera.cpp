f67c6333a3bd96a55601996e6ba3be9e1085ea0c
hexbabe <sean.xiaochen.yu@gmail.com>
2025-09-18 11:09:47 -0400

    Make unused params anon and add comment

--- csi_camera.cpp
+++ csi_camera.cpp
@@ -70,7 +70,9 @@ Camera::raw_image CSICamera::get_image(const std::string mime_type, const ProtoS
     return image;
 }
 
-Camera::image_collection CSICamera::get_images(std::vector<std::string> filter_source_names, const ProtoStruct& extra) {
+Camera::image_collection CSICamera::get_images(std::vector<std::string> /* filter_source_names */, const ProtoStruct& /* extra */) {
+    // filter_source_names and extra are unused because this camera provides a single image source
+    // and doesn't include any extra support for get_images
     ProtoStruct empty_extra;
     // If image is not available, an exception will be thrown
     raw_image image = get_image(DEFAULT_OUTPUT_MIMETYPE, empty_extra);
